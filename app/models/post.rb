afc79e02601cf28140812928a1024ba57d234254
tatiana.mojica <tatiana.mojica@bustle.com>
2019-05-06 02:47:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 200}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait?
+
+  def clickbait?
+    if self.title && !self.title.match(/Won't Believe|Secret|Top [\d*]|Guess/)
+      errors.add(:clickbait, "is not enticing enough")
+    end
+  end
 end
+
